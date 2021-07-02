0121272a7e7d0a3892f92aca14e3d76b8ade3527
starbvuks <starbvuks@gmail.com>
2021-07-02 17:19:56 +0530

    card-styles

--- mmdb/src/components/Cards/MovieCardStyles.js
+++ mmdb/src/components/Cards/MovieCardStyles.js
@@ -5,6 +5,8 @@ export const Container = styled.div`
   justify-content: center;
   display: flex;
   margin: 3% 5% 0 5%;
+  border-radius: 10px;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.1);
   width: 300px;
   height: auto;
 `;
@@ -13,10 +15,15 @@ export const CardContent = styled.div``;
 
 export const ImageDiv = styled.div``;
 
-export const Poster = styled.img``;
+export const Poster = styled.img`
+  border-radius: 10px 10px 0px 0px;
+`;
 
 export const BottomPortion = styled.div`
-  margin: 10px 0 10px 15px;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const Title = styled.span`
@@ -33,9 +40,10 @@ export const Rating = styled.span`
   color: #2f3b3e;
   font-family: "Montserrat";
   background-color: #c3d8c8;
+
   letter-spacing: 1px;
   font-weight: 700;
-  font-size: 24px;
+  font-size: 18px;
   padding: 5px;
   border-radius: 10px;
 `;
