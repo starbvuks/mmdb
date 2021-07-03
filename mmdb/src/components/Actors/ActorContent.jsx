ee6b8a5384821c2f38c27cbdba7f1725b41ca743
starbvuks <starbvuks@gmail.com>
2021-07-04 01:29:07 +0530

    actor-classes

--- mmdb/src/components/Actors/ActorContent.jsx
+++ mmdb/src/components/Actors/ActorContent.jsx
@@ -8,14 +8,16 @@ function ActorContent({name, profile_path}) {
     <Container>
       <Content>
         <Poster>
-          <Image src={IMAGE_API + profile_path} alt="movie" />
+          <Image
+            className="img-wrap"
+            src={IMAGE_API + profile_path}
+            alt="movie"
+          />
         </Poster>
-        <Title>{name}</Title>
+        <Title className="img-desc">{name}</Title>
       </Content>
     </Container>
   );
 }
 
 export default ActorContent;
-
-// https://www.themoviedb.org/t/p/w200//vSIuCJNvXhgVg0DwuHa0bPl5EeS.jpg
